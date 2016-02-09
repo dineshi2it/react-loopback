f46b40b2311a6f7d1c149a39bdcf2f6be7064292
dineshi2it <dinesh.r@ideas2it.com>
2016-02-09 17:57:37 +0530

    Feature: Gulp & react components added 	- Browserify 	- reactify 	- bundle js created 	references: 		- https://github.com/RAJESHI2IT/SailsReact/tree/master/api 		- https://gist.github.com/Sigmus/9253068

--- client/app.js
+++ client/app.js
@@ -1,43 +1,25 @@
-  /**
- * Copyright (c) 2014, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
 var React = require('react');
 
-var ReactRouter = require('react-router')
+var ReactRouter = require('react-router');
 var DefaultRoute = ReactRouter.DefaultRoute;
 var Link = ReactRouter.Link;
 var Route = ReactRouter.Route;
 var RouteHandler = ReactRouter.RouteHandler;
 
-var ProductData = require('./ProductData');
-var CartAPI = require('./utils/CartAPI')
-var FluxCartApp = require('./components/FluxCartApp.react');
-var DisplayList = require('./components/DisplayList.react');
-
-
-// Load Mock Product Data into localStorage
-ProductData.init();
+var API = require('./utils/API');
+var UserApp = require('./components/UserApp.react');
+var Profile = require('./components/Profile.react');
 
 // Load Mock API Call
-CartAPI.getProductData();
+API.getUserList();
 
 var Index = React.createClass({
   render: function () {
     return (
       <div>
-        <div>
-        <Link to="/">Sample Page</Link>
-        <Link to="/route">Route</Link>
-        <a href="/register">Register</a>
-        <a href="/login">Login</a>
+		    <div className="container">
+          <RouteHandler />
         </div>
-        <RouteHandler />
       </div>
     );
   }
@@ -45,11 +27,11 @@ var Index = React.createClass({
 
 var routes = (
   <Route path="/" handler={Index}>
-    <Route path="route" handler={DisplayList}/>
-    <DefaultRoute handler={FluxCartApp}/>
+  <Route path="/profile" handler={Profile} />
+    <DefaultRoute handler={UserApp}/>
   </Route>
 );
 
 ReactRouter.run(routes, function (Handler) {
-  React.render(<Handler/>, document.getElementById('todoapp'));
+  React.render(<Handler/>, document.getElementById('reactRender'));
 });
