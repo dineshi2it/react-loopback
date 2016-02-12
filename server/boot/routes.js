bd0183020838c37f2b4a136f5331f4d0b3fe4f18
dineshi2it <dinesh.r@ideas2it.com>
2016-02-12 11:36:09 +0530

    Fix: Code cleanup server

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -6,54 +6,20 @@ module.exports = function routes(app) {
 
   app.get('/', function (req, res) {
 
-     console.log("=======1=================",req.url);
     var router = Router.create({
       location: req.url,
       routes: reactRoutes
     });
 
     router.run(function (Handler) {
-			var model = {"name":"XXXXXXXXXXXXXXXXXXXXXX"};
-
-			
-        var html = React.renderToString(
-          <Handler bootstrap = {{model}} />
-        );
-
-        res.render('index', {
-          markup: html,
-          bootstrap: JSON.stringify(model)
-        });
-
-    });
-  });
-
-  app.get('/profile', function (req, res) {
-
-    console.log(reactRoutes,"========================",req.url);
-    var router = Router.create({
-      location: req.url,
-      routes: reactRoutes
-    });
-
-    router.run(function (Handler) {
-      var model = {"name":"XXXXXXXXXXXXXXXXXXXXXX"};
-      
-      
-        var html = React.renderToString(
-          <Handler bootstrap={{model}} />
-        );
-
-
-        console.log("===================HTML===================");
-        console.log(html);
-        console.log("===================HTML===================");
-
-        res.render('index', {
-          markup: html,
-          bootstrap: JSON.stringify(model)
-        });
-
+			var model = {"data":"test"};
+      var html = React.renderToString(
+        <Handler bootstrap = {{model}} />
+      );
+      res.render('index', {
+        markup: html,
+        bootstrap: JSON.stringify(model)
+      });
     });
   });
 }
